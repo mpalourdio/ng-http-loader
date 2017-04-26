a06bfe140d5afa5d8be55505136be54d905601d6
Michel Palourdio <mpalourdio@gmail.com>
2017-04-26 18:44:26 +0200

    Add tests for SpinnerComponent

--- src/app/spinner/spinner.component.spec.ts
+++ src/app/spinner/spinner.component.spec.ts
@@ -7,10 +7,12 @@
  * CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
  */
 
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { async, ComponentFixture, fakeAsync, TestBed, tick } from '@angular/core/testing';
 import { SpinnerComponent } from './spinner.component';
 import { HttpInterceptorServiceFactoryProvider } from '../http-interceptor.service';
 import { HttpModule } from '@angular/http';
+import { By } from '@angular/platform-browser';
+import { Spinkit } from '../spinkits';
 
 describe('SpinnerComponent', () => {
     let component: SpinnerComponent;
@@ -34,4 +36,55 @@ describe('SpinnerComponent', () => {
     it('should create the spinner component', () => {
         expect(component).toBeTruthy();
     });
+
+    it('should create the spinner component with default values', fakeAsync(() => {
+        component.isSpinnerVisible = true;
+        fixture.detectChanges();
+        tick();
+
+        const element = fixture
+            .debugElement
+            .query(By.css('.sk-cube-grid'))
+            .nativeElement;
+
+        expect(element.className).toBe('sk-cube-grid colored');
+    }));
+
+    it('should not set the colored class if background-color is defined', fakeAsync(() => {
+        component.isSpinnerVisible = true;
+        component.backgroundColor = '#ff0000';
+
+        fixture.detectChanges();
+        tick();
+
+        const element = fixture
+            .debugElement
+            .query(By.css('.sk-cube-grid'))
+            .nativeElement;
+
+        expect(element.className).toBe('sk-cube-grid');
+    }));
+
+    it('should not display anything by default', fakeAsync(() => {
+        const element = fixture
+            .debugElement
+            .query(By.css('#http-loader'));
+
+        expect(element).toBeNull();
+    }));
+
+    it('should be able to specify another known spinner', fakeAsync(() => {
+        component.isSpinnerVisible = true;
+        component.spinner = Spinkit.skRotatingPlane;
+
+        fixture.detectChanges();
+        tick();
+
+        const element = fixture
+            .debugElement
+            .query(By.css('.sk-rotating-plane'))
+            .nativeElement;
+
+        expect(element.className).toBe('sk-rotating-plane colored-parent');
+    }));
 });
