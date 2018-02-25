66bb03639c33b133fc9cb3802c7c911652bd9ce3
Michel Palourdio <mpalourdio@gmail.com>
2018-02-25 09:32:23 +0100

    Explicit return type for PendingInterceptorServiceFactory

--- src/services/pending-interceptor.service.ts
+++ src/services/pending-interceptor.service.ts
@@ -69,7 +69,7 @@ export class PendingInterceptorService implements HttpInterceptor {
     }
 }
 
-export function PendingInterceptorServiceFactory() {
+export function PendingInterceptorServiceFactory(): PendingInterceptorService {
     return new PendingInterceptorService();
 }
 
