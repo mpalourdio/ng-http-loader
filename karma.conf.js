2cd50b9fe062d4ed130e6418a353d42dc004d937
Michel Palourdio <mpalourdio@gmail.com>
2018-05-25 07:49:26 +0200

    Fix karma basePath option so coverage can be displayed in IntelliJ

--- karma.conf.js
+++ karma.conf.js
@@ -3,7 +3,7 @@
 
 module.exports = function (config) {
   config.set({
-    basePath: 'test',
+    basePath: '',
     frameworks: ['jasmine', '@angular-devkit/build-angular'],
     plugins: [
       require('karma-jasmine'),
