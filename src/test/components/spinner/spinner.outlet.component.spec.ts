f2182c02da06341c195037af32e605e9e34636d9
Michel Palourdio <mpalourdio@gmail.com>
2018-06-09 08:15:20 +0200

    Remove useless imports

--- src/test/components/spinner/spinner.outlet.component.spec.ts
+++ src/test/components/spinner/spinner.outlet.component.spec.ts
@@ -8,9 +8,7 @@
  */
 
 import { HttpClientTestingModule } from '@angular/common/http/testing';
-import { Component } from '@angular/core';
-import { NgModule } from '@angular/core/src/metadata/ng_module';
-import { async, ComponentFixture, MetadataOverride, TestBed } from '@angular/core/testing';
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { By } from '@angular/platform-browser';
 import { BrowserDynamicTestingModule } from '@angular/platform-browser-dynamic/testing';
 import { SkThreeBounceComponent } from '../../../lib/components/sk-three-bounce/sk-three-bounce.component';
