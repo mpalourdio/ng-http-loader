27248b3b47634d34af523950f3d7f5b84bfd5bfb
Michel Palourdio <mpalourdio@gmail.com>
2018-10-19 14:24:33 +0200

    Remove reflect from polyfills

--- polyfills.ts
+++ polyfills.ts
@@ -45,7 +45,7 @@ import 'core-js/es6/set';
 // import 'classlist.js';  // Run `npm install --save classlist.js`.
 
 /** IE10 and IE11 requires the following for the Reflect API. */
-import 'core-js/es6/reflect';
+// import 'core-js/es6/reflect';
 
 /**
  * Web Animations `@angular/platform-browser/animations`
