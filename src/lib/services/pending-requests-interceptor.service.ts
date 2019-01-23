4600f5064d6c1c92addb969147578d0b298eaa87
Michel Palourdio <mpalourdio@gmail.com>
2019-01-23 20:31:57 +0100

    Remove braces

--- src/lib/services/pending-requests-interceptor.service.ts
+++ src/lib/services/pending-requests-interceptor.service.ts
@@ -91,9 +91,7 @@ export class PendingRequestsInterceptor implements HttpInterceptor {
         }
 
         return next.handle(req).pipe(
-            catchError((error: HttpErrorResponse) => {
-                return throwError(error);
-            }),
+            catchError((error: HttpErrorResponse) => throwError(error)),
             finalize(() => {
                 if (!shouldBypass) {
                     this._pendingRequests--;
