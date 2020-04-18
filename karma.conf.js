fa0dcc87ef8ea7a4f0aa9faa1c70928e67d4b9a2
Michel Palourdio <mpalourdio@gmail.com>
2020-04-18 12:55:34 +0200

    Bump karma and use puppeteer form chromium tests

--- karma.conf.js
+++ karma.conf.js
@@ -1,6 +1,9 @@
 // Karma configuration file, see link for more information
 // https://karma-runner.github.io/1.0/config/configuration-file.html
 
+const process = require('process');
+process.env.CHROMIUM_BIN = require('puppeteer').executablePath();
+
 module.exports = function (config) {
   config.set({
     basePath: '',
